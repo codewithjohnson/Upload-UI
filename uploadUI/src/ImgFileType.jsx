5ac6710a2596a28649ea1bcbbb733c1d08e5e1e6
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-10-04 19:44:33 +0100

    added m4 icon

--- uploadUI/src/ImgFileType.jsx
+++ uploadUI/src/ImgFileType.jsx
@@ -2,10 +2,12 @@ import blank from "./assets/blank.png";
 import png from "./assets/css.png";
 import pdf from "./assets/pdf.png";
 import css from "./assets/css.png";
+import mp4 from "./assets/mp4.png";
 
 export const ImageFileFormat = {
   default: blank,
   css: css,
   png: png,
+  mp4: mp4,
   pdf: pdf,
 };
